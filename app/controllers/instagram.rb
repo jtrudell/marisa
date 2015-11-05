843982fefd99d698f83386d238e468140a3780ed
Jeanette Trudell <jtrudell@gmail.com>
2015-11-05 14:48:16 -0600

    add instagram link

--- app/controllers/instagram.rb
+++ app/controllers/instagram.rb
@@ -8,15 +8,11 @@ Instagram.configure do |config|
   config.client_secret = "b6a9c720d745491e890aa8302d636fe1"
 end
 
-get "/oauth" do
-  '<a href="/oauth/connect">Connect with Instagram</a>'
-end
-
-get "/oauth/connect" do
+get "/instagram/oauth" do
   redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
 end
 
-get "/oauth/callback" do
+get "/instagram/oauth/callback" do
   response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
   session[:access_token] = response.access_token
   redirect "/nav"
