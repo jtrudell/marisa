c5f87b2a267a5ec9282c263c00d8a63250f3e713
Jeanette Trudell <jtrudell@gmail.com>
2015-11-06 15:24:19 -0600

    i can't stop

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,22 +1,5 @@
 require 'bing-search'
 
-# def find_articles
-#   BingSearch.account_key = 'l1VfVxiJeygTtAUbcXL8O7A5Wpt3bmvH3+Z0eqX6HOE'
-#   results = []
-#   yoga_results = []
-#   shopping_results = []
-#   yoga_results << BingSearch.web('yoga')
-#   # results << (BingSearch.web('travel'))[rand(10)]
-#   # results << (BingSearch.web('cats'))[rand(10)]
-#   shopping_results << BingSearch.web('shopping')
-#   # results << (BingSearch.web('meditation'))[rand(10)]
-#   results << yoga_results[rand(10)]
-#   results << yoga_results[rand(10)]
-#   results << shopping_results[rand(10)]
-#   results << shopping_results[rand(10)]
-#   results.shuffle
-# end
-
 def find_articles
   BingSearch.account_key = 'l1VfVxiJeygTtAUbcXL8O7A5Wpt3bmvH3+Z0eqX6HOE'
   results = []
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,22 @@
+$(document).ready(function() {
+
+  $("#click-tags li a").on("click", getImages);
+
+  function getImages(event){
+    event.preventDefault();
+    var url = $(this).attr("href");
+    var request = $.ajax({
+      url: url,
+      method: "get",
+    });
+
+    request.done(function(response) {
+      $("#main-content-right").remove();
+      $("#last-minute-hack").append(response);
+    });
+  };
+});
+
 
 // TWITTER SHARE
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
