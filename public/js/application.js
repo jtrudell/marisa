19e1c73778db86a266eacda4b64d341160bde60b
Jeanette Trudell <jtrudell@gmail.com>
2015-11-06 14:40:25 -0600

    hopefully the end of css

--- public/js/application.js
+++ public/js/application.js
@@ -1,31 +1,3 @@
-$(document).ready(function() {
-
-
-// var getInstagramId = $.ajax({
-//   url: "https://api.instagram.com/v1/users/search?q=msg0927&client_id=a963ba34ecb443bcbaeafa815a9154a9",
-//   data: {"access_token": "a963ba34ecb443bcbaeafa815a9154a9",
-//           "App Secret": "b6a9c720d745491e890aa8302d636fe1"},
-//   headers: {
-//         'access-control-allow-origin':'*',
-//     },
-//     dataType: 'JSON',
-//   method: "get"
-//
-// });
-//
-// var instagramId = getInstagramId.done(function(response){
-//   return response;
-// });
-//
-//   $.ajax({
-//     url: "https://api.instagram.com/v1/users/" + instagramId + "/media/recent/",
-//     method: "get",
-//     headers: {
-//           'access-control-allow-origin':'*',
-//       }
-//   });
-
-});
 
 // TWITTER SHARE
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
