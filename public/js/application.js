b64174197e45b3995f79bf6132b739a7cf46e68f
Jen Trudell <jtrudell@gmail.com>
2015-11-05 22:08:23 -0600

    still messing with instagram api

--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
 
 
-var getInstagramId = $.ajax({
-  url: "https://api.instagram.com/v1/users/search?q=msg0927&client_id=a963ba34ecb443bcbaeafa815a9154a9",
-  method: "get"
-});
-
-var instagramId = getInstagramId.done(function(response){
-  return response;
-});
-
-console.log(instagramId);
-
+// var getInstagramId = $.ajax({
+//   url: "https://api.instagram.com/v1/users/search?q=msg0927&client_id=a963ba34ecb443bcbaeafa815a9154a9",
+//   data: {"access_token": "a963ba34ecb443bcbaeafa815a9154a9",
+//           "App Secret": "b6a9c720d745491e890aa8302d636fe1"},
+//   headers: {
+//         'access-control-allow-origin':'*',
+//     },
+//     dataType: 'JSON',
+//   method: "get"
+//
+// });
+//
+// var instagramId = getInstagramId.done(function(response){
+//   return response;
+// });
+//
 //   $.ajax({
 //     url: "https://api.instagram.com/v1/users/" + instagramId + "/media/recent/",
-//     method: "get"
-//   })
-//
+//     method: "get",
+//     headers: {
+//           'access-control-allow-origin':'*',
+//       }
+//   });
+
 });
 
 // TWITTER SHARE
