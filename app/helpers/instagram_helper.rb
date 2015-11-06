a92cbf9cb51c4dc82e1b3515914c8c724083ad89
Jeanette Trudell <jtrudell@gmail.com>
2015-11-05 18:01:00 -0600

    more styling

--- app/helpers/instagram_helper.rb
+++ app/helpers/instagram_helper.rb
@@ -10,7 +10,7 @@ def generate_instagram_images
   for media_item in client.tag_recent_media(tags[0].name)
     img_src_array << "<a href = '#{media_item.images.standard_resolution.url}'><img src='#{media_item.images.thumbnail.url}'></a>"
   end
-  6.times do
+  12.times do
     html << img_src_array.shuffle.pop
   end
   return html
