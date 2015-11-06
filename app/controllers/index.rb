ebfedfcfef7dfdcb5e1bf43bc2ac0be0f5d5c049
Jen Trudell <jtrudell@gmail.com>
2015-11-05 22:14:23 -0600

    more instagram

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -39,12 +39,7 @@ get "/nav" do
     <ol>
       <li><a href='/user_recent_media'>User Recent Media</a> Calls user_recent_media - Get a list of a user's most recent media</li>
       <li><a href='/user_media_feed'>User Media Feed</a> Calls user_media_feed - Get the currently authenticated user's media feed uses pagination</li>
-      <li><a href='/location_recent_media'>Location Recent Media</a> Calls location_recent_media - Get a list of recent media at a given location, in this case, the Instagram office</li>
-      <li><a href='/media_search'>Media Search</a> Calls media_search - Get a list of media close to a given latitude and longitude</li>
-      <li><a href='/media_popular'>Popular Media</a> Calls media_popular - Get a list of the overall most popular media items</li>
       <li><a href='/user_search'>User Search</a> Calls user_search - Search for users on instagram, by name or username</li>
-      <li><a href='/location_search'>Location Search</a> Calls location_search - Search for a location by lat/lng</li>
-      <li><a href='/location_search_4square'>Location Search - 4Square</a> Calls location_search - Search for a location by Fousquare ID (v2)</li>
       <li><a href='/tags'>Tags</a>Search for tags, view tag info and get media by tag</li>
       <li><a href='/limits'>View Rate Limit and Remaining API calls</a>View remaining and ratelimit info.</li>
     </ol>
@@ -62,17 +57,6 @@ get "/user_recent_media" do
   html
 end
 
-get '/media_like/:id' do
-  client = Instagram.client(:access_token => session[:access_token])
-  client.like_media("#{params[:id]}")
-  redirect "/user_recent_media"
-end
-
-get '/media_unlike/:id' do
-  client = Instagram.client(:access_token => session[:access_token])
-  client.unlike_media("#{params[:id]}")
-  redirect "/user_recent_media"
-end
 
 get "/user_media_feed" do
   client = Instagram.client(:access_token => session[:access_token])
@@ -93,59 +77,16 @@ get "/user_media_feed" do
   html
 end
 
-get "/location_recent_media" do
-  client = Instagram.client(:access_token => session[:access_token])
-  html = "<h1>Media from the Instagram Office</h1>"
-  for media_item in client.location_recent_media(514276)
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
-  end
-  html
-end
-
-get "/media_search" do
-  client = Instagram.client(:access_token => session[:access_token])
-  html = "<h1>Get a list of media close to a given latitude and longitude</h1>"
-  for media_item in client.media_search("37.7808851","-122.3948632")
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
-  end
-  html
-end
-
-get "/media_popular" do
-  client = Instagram.client(:access_token => session[:access_token])
-  html = "<h1>Get a list of the overall most popular media items</h1>"
-  for media_item in client.media_popular
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
-  end
-  html
-end
 
 get "/user_search" do
   client = Instagram.client(:access_token => session[:access_token])
   html = "<h1>Search for users on instagram, by name or usernames</h1>"
-  for user in client.user_search("instagram")
+  for user in client.user_search("msg0927")
     html << "<li> <img src='#{user.profile_picture}'> #{user.username} #{user.full_name}</li>"
   end
   html
 end
 
-get "/location_search" do
-  client = Instagram.client(:access_token => session[:access_token])
-  html = "<h1>Search for a location by lat/lng with a radius of 5000m</h1>"
-  for location in client.location_search("48.858844","2.294351","5000")
-    html << "<li> #{location.name} <a href='https://www.google.com/maps/preview/@#{location.latitude},#{location.longitude},19z'>Map</a></li>"
-  end
-  html
-end
-
-get "/location_search_4square" do
-  client = Instagram.client(:access_token => session[:access_token])
-  html = "<h1>Search for a location by Fousquare ID (v2)</h1>"
-  for location in client.location_search("3fd66200f964a520c5f11ee3")
-    html << "<li> #{location.name} <a href='https://www.google.com/maps/preview/@#{location.latitude},#{location.longitude},19z'>Map</a></li>"
-  end
-  html
-end
 
 get "/tags" do
   client = Instagram.client(:access_token => session[:access_token])
